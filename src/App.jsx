796fcbcbce1d1a5e99aa286511fa75d6f280458e
nadasoliman2 <nadaemad271@gmail.com>
2025-08-12 12:58:05 +0300

    fix: prevent form reload on add comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,35 @@
-import { useState } from 'react'
 import {
   createBrowserRouter,
   RouterProvider
 } from 'react-router-dom';
-import './App.css';
 import Layout from './Commponet/Layout';
-import { Children } from 'react';
 import Home from './pages/home';
 import Notfound from './pages/Notfound';
 import Login from './pages/Login';
 import Register from './pages/register';
 import { Singlepost } from './pages/singlepost';
 import Profile from './pages/Profile';
+
 function App() {
-
-
-
-  const x = createBrowserRouter([
-      
+  const router = createBrowserRouter(
+    [
       {
         path: '/',
         element: <Layout />,
         errorElement: <Notfound/>, 
         children: [
-          
-  
-          {path : '/', element: <Login></Login>},
-                    {path :'/home', element: <Home />},
-                       { path: '/posts/:id', element:<Singlepost></Singlepost>},
-                            {path :'/register', element: <Register />},
-                           {path :'/profile/:id', element: <Profile></Profile>},
-
-
-
-     
-  
-  
+          { path: '/', element: <Login /> },
+          { path: '/home', element: <Home /> },
+          { path: '/posts/:id', element: <Singlepost /> },
+          { path: '/register', element: <Register /> },
+          { path: '/profile/:id', element: <Profile /> },
         ]
       }
-    ]);
-  return (
-    <>
-   <RouterProvider router={x}></RouterProvider>
-  
-    </>
-  )
+    ],
+    { basename: '/socialapp' } // هنا ضبط الـ basename
+  );
+
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Theme } from './context/theme.context.jsx';
 import App from './App.jsx'
 import { Authcontext } from './context/auth.login.jsx';
+import { BrowserRouter } from 'react-router-dom';
 const queryClient = new QueryClient({
     defaultOptions:{
         queries:{
@@ -19,7 +20,9 @@ createRoot(document.getElementById('root')).render(
             <Toaster></Toaster>
     <Authcontext>
  <Theme>
+       <BrowserRouter basename="/socialapp"></BrowserRouter>
     <App />
+    
 </Theme>
 </Authcontext>
 </QueryClientProvider>
